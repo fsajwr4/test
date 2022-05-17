59957b5abe1d2a73f85db7e6e5aa4c095e65ab62
Q539218 <magnus.wojahn@bmw.de>
2022-05-17 11:18:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+string = "[100. 300.]"
+
+string = string.replace(string[0], "", 1)
+string = string.replace(string[3], ",", 1)
+string = string.replace(string[4], "", 1)
+string = string.replace(string[8], "", 1)
+string = string.replace(string[7], "", 1)
+
+
+
+
+
+print(string)
